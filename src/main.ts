a829041119772fad3885020b90ae65f2ae091036
Eray Aydın <erayaydinn@protonmail.com>
2022-07-03 06:48:08 +0300

    feat: example `main.ts` file in source directory for testing library

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
 #!/usr/bin/env node
+
+import type { IncomingMessage, ServerResponse } from 'http';
+import { createServer } from 'http';
+
+createServer((request: IncomingMessage, response: ServerResponse) => {
+  response.end('OK');
+}).listen(process.env.PORT || '8080');
